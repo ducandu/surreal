Implement Gumbel-Softmax deterministic sampling (simply use distribution's probs) and fix test case.

surreal/components/distributions/gumbel_softmax.py
@@ -45,9 +45,10 @@ class GumbelSoftmax(Distribution):
         return tfp.distributions.RelaxedOneHotCategorical(temperature=self.temperature, logits=parameters)
 
     def _sample_deterministic(self, distribution):
-        raise NotImplementedError
-        ## Cast to float again because this is called from a tf.cond where the other option calls a stochastic
-        ## sample returning a float.
+        # Simply return probs.
+        return distribution._distribution.probs
+        # Cast to float again because this is called from a tf.cond where the other option calls a stochastic
+        # sample returning a float.
         #argmax = tf.argmax(input=distribution._distribution.probs, axis=-1, output_type=tf.int32)
         #sample = tf.cast(argmax, dtype=tf.float32)
         ## Argmax turns (?, n) into (?,), not (?, 1)

surreal/tests/components/test_distributions.py
@@ -422,7 +422,7 @@ class TestDistributions(unittest.TestCase):
 
         # Batch of size=2 and deterministic (True).
         input_ = param_space.sample(2)
-        expected = np.argmax(input_, axis=-1)
+        expected = softmax(input_)
         # Sample n times, expect always argmax value (deterministic draw).
         for _ in range(50):
             out = gumble_softmax_distribution.sample(input_, deterministic=True)
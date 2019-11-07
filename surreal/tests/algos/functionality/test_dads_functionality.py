bf0fad4620226444efc5935e8a130e212e85754d
sven1977 <svenmika1977@gmail.com>
2019-11-07 22:04:13 +0100

    Add DADS functionality test (only "compile" for now) to travis.

--- surreal/tests/algos/functionality/test_dads_functionality.py
+++ surreal/tests/algos/functionality/test_dads_functionality.py
@@ -1,4 +1,4 @@
-# Copyright 2019 ducandu GmbH. All Rights Reserved.
+# Copyright 2019 ducandu GmbH, All Rights Reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,4 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
+import logging
+import os
+import unittest
+
+from surreal.algos.dads import DADS, DADSConfig
+from surreal.envs import GridWorld
+
+
+class TestDADSFunctionality(unittest.TestCase):
+    """
+    Tests the SAC algo functionality (loss functions, execution logic, etc.).
+    """
+    logging.getLogger().setLevel(logging.INFO)
+
+    def test_dads_compilation(self):
+        """
+        Tests the c'tor of SAC.
+        """
+        env = GridWorld("4-room", actors=2)
+        # Create a Config (for any Atari game).
+        config = DADSConfig.make(
+            "{}/../configs/dads_grid_world_4room_learning.json".format(os.path.dirname(__file__)),
+            state_space=env.actors[0].state_space,
+            action_space=env.actors[0].action_space
+        )
+        dads = DADS(config, name="my-dads")
+        print("DADS built ({}).".format(dads))
 
